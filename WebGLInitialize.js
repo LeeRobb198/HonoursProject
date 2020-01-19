55aca948695406d7484d4e86bb5b46f488404c9d
Lee Robbie <leerobb198@lees-mbp.home>
2020-01-19 14:48:47 +0000

    Issue commit

--- WebGLInitialize.js
+++ WebGLInitialize.js
@@ -13,70 +13,72 @@ function initialize() {
 
         var url = "https://opensky-network.org/api/states/all";
 
-        // Return Specific States (Leaflet Map) --------------------------------
+        // Return Specific States (WebGL Map) ----------------------------------
 
-        $.getJSON( url , function(data) {
+        setInterval(function(){
 
-          var result = [];
-          var newData = [];
-          var newArray = [];
-          var testArray = [];
-          var marker;
+          $.getJSON( url , function(data) {
 
-          result['times'] = data.time;
+            var result = [];
+            var newData = [];
+            var newArray = [];
+            var testArray = [];
+            var marker;
 
-          var icao24, callsign, origin_country, time_position, last_contact,
-              longitude, latitude, baro_altitude, on_ground, velocity,
-              true_track, vertical_rate, sensors, geo_altitude, squawk,
-              spi, position_source;
+            result['times'] = data.time;
 
-          for(var i = 0; i < data.states.length; i++) {
-            if(data.states[i][2] == 'United States') {
-              icao24 = data.states[i][0];
-              callsign = data.states[i][1];
-              origin_country = data.states[i][2];
-              time_position = data.states[i][3];
-              last_contact = data.states[i][4];
-              longitude = data.states[i][5];
-              latitude = data.states[i][6];
-              baro_altitude = data.states[i][7];
-              on_ground = data.states[i][8];
-              velocity = data.states[i][9];
-              true_track = data.states[i][10];
-              vertical_rate = data.states[i][11];
-              sensors = data.states[i][12];
-              geo_altitude = data.states[i][13];
-              squawk = data.states[i][14];
-              spi = data.states[i][15];
-              position_source = data.states[i][16];
+            var icao24, callsign, origin_country, time_position, last_contact,
+                longitude, latitude, baro_altitude, on_ground, velocity,
+                true_track, vertical_rate, sensors, geo_altitude, squawk,
+                spi, position_source;
 
-              newData = [icao24, callsign, origin_country, time_position, last_contact,
-                  longitude, latitude, baro_altitude, on_ground, velocity,
-                  true_track, vertical_rate, sensors, geo_altitude, squawk,
-                  spi, position_source];
+            for(var i = 0; i < data.states.length; i++) {
+              if(data.states[i][2] == 'United States') {
+                icao24 = data.states[i][0];
+                callsign = data.states[i][1];
+                origin_country = data.states[i][2];
+                time_position = data.states[i][3];
+                last_contact = data.states[i][4];
+                longitude = data.states[i][5];
+                latitude = data.states[i][6];
+                baro_altitude = data.states[i][7];
+                on_ground = data.states[i][8];
+                velocity = data.states[i][9];
+                true_track = data.states[i][10];
+                vertical_rate = data.states[i][11];
+                sensors = data.states[i][12];
+                geo_altitude = data.states[i][13];
+                squawk = data.states[i][14];
+                spi = data.states[i][15];
+                position_source = data.states[i][16];
 
-              newArray.push(newData);
+                newData = [icao24, callsign, origin_country, time_position, last_contact,
+                    longitude, latitude, baro_altitude, on_ground, velocity,
+                    true_track, vertical_rate, sensors, geo_altitude, squawk,
+                    spi, position_source];
 
-              if(latitude !== null && longitude !== null) {
-                // WE.marker([latitude, longitude], {icon: planeIcon}).addTo(map_3D)
-                WE.marker([latitude, longitude]).addTo(earth)
-                .bindPopup( "<b>Flight Data</b>" +
-                            "<br>ICAO 24-bit Address: " + icao24 +
-                            "<br>Origin Country: " + origin_country +
-                            "<br>Latitude: " + latitude +
-                            "<br>Longitude: " + longitude +
-                            "<br>Velocity: " + velocity, {maxWidth: 150, closeButton: true});
+                newArray.push(newData);
+
+                if(latitude !== null && longitude !== null) {
+                  // WE.marker([latitude, longitude], {icon: planeIcon}).addTo(map_3D)
+                  WE.marker([latitude, longitude]).addTo(earth)
+                  .bindPopup( "<b>Flight Data</b>" +
+                              "<br>ICAO 24-bit Address: " + icao24 +
+                              "<br>Origin Country: " + origin_country +
+                              "<br>Latitude: " + latitude +
+                              "<br>Longitude: " + longitude +
+                              "<br>Velocity: " + velocity, {maxWidth: 150, closeButton: true});
+                }
               }
+
             }
 
-          }
+            result['states'] = newArray;
+            // console.log("Third console");
+            // console.log(result);
 
-          result['states'] = newArray;
-          console.log("Third console");
-          console.log(result);
-
-        });
-
+          });
+        }, 10000); // Updates every 5 seconds
         // Icon ----------------------------------------------------------------
 
       //   var planeIcon = L.icon({
