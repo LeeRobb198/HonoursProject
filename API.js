e84f7dc725985e37006f278a24de124841b3850c
Lee Robbie <leerobb198@lees-mbp.home>
2020-01-20 00:07:44 +0000

    Introduction of search parameters

--- API.js
+++ API.js
@@ -12,6 +12,272 @@ var url = "https://opensky-network.org/api/states/all";
 
       console.log(data.states.length);
 
+      // Test ------------------------------------------------------------------
+
+      var unitedStates, chile, austria, barbados, spain, hungary,
+          southAfrica, canada, germany, sweden, brazil, iceland,
+          estonia, australia, thailand, unitedKingdom;
+
+      // Finding all countries
+
+      for(var i = 0; i < data.states.length; i++) {
+        if(data.states[i][2] == 'United States') {
+          unitedStates++;
+        } else if(data.states[i][2] == 'Chile') {
+          chile++;
+        } else if(data.states[i][2] == 'Austria') {
+          austria++;
+        } else if(data.states[i][2] == 'Barbados') {
+          barbados++;
+        } else if(data.states[i][2] == 'Spain') {
+          spain++;
+        } else if(data.states[i][2] == 'Hungary') {
+          hungary++;
+        } else if(data.states[i][2] == 'South Africa') {
+          southAfrica++;
+        } else if(data.states[i][2] == 'Canada') {
+          canada++;
+        } else if(data.states[i][2] == 'Germany') {
+          germany++;
+        } else if(data.states[i][2] == 'Sweden') {
+          sweden++;
+        } else if(data.states[i][2] == 'Brazil') {
+          brazil++;
+        } else if(data.states[i][2] == 'Iceland') {
+          iceland++;
+        } else if(data.states[i][2] == 'Estonia') {
+          estonia++;
+        } else if(data.states[i][2] == 'Australia') {
+          australia++;
+        } else if(data.states[i][2] == 'Thailand') {
+          thailand++;
+        } else if(data.states[i][2] == 'New Zealand') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Indonesia') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Norway') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Slovakia') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'France') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Argentina') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Italy') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Poland') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'China') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'India') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Colombia') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Czech Republic') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Iraq') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Bangladesh') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Japan') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Israel') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Turkey') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'United Kingdom') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Kingdom of the Netherlands') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'United Arab Emirates') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Denmark') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Belgium') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'San Marino') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Morocco') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Islamic Republic of Iran') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Russian Federation') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Brunei Darussalam') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Kazakhstan') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Luxembourg') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Jordan') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Libyan Arab Jamahiriya') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Finland') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'United Kingdom') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Mexico') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Malaysia') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Portugal') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Serbia') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Singapore') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Albania') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Bahrain') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Lebanon') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Taiwan') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Togo') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Ireland') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Ethiopia') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Ukraine') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Latvia') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Republic of Korea') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Qatar') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Trinidad and Tobago') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Lithuania') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Myanmar') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Algeria') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Belarus') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Fiji') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Republic of Moldova') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Azerbaijan') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Bolivia') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Switzerland') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Lao People&#39;s Democratic Republic') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Dominican Republic') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Romania') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Philippines') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Bulgaria') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Mongolia') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Slovenia') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Greece') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Tunisia') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Saudi Arabia') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Mauritius') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Pakistan') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Sri Lanka') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Oman') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Viet Nam') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Croatia') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Kuwait') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Venezuela') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Tajikistan') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Turkmenistan') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Uzbekistan') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Egypt') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Georgia') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Malta') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Cape Verde') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Nepal') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Kenya') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Cambodia') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Panama') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Armenia') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Maldives') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Guatemala') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Peru') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Papua New Guinea') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Montenegro') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Antigua and Barbuda') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Mauritania') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Ecuador') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Paraguay') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Bahamas') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Costa Rica') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Vanuatu') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Nigeria') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Seychelles') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Senegal') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Afghanistan') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Angola') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Uruguay') {
+          unitedKingdom++;
+        } else if(data.states[i][2] == 'Syrian Arab Republic') {
+          unitedKingdom++;
+        }
+
+        else {
+          console.log(data.states[i][2]);
+        }
+      }
+
+
+
+      console.log("Complete");
+
       // Returns 1 states first value
       //console.log(data.states[0][2]);
   });
--- Map.js
+++ Map.js
@@ -8,7 +8,6 @@
 var map = L.map('map_2D', {minZoom: 2, maxBoundsViscosity: 1.0}).fitWorld();
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
     accessToken: 'pk.eyJ1IjoibGVlcm9iYjE5OCIsImEiOiJjazU3ZGIwam8wMDlxM2VxaDg3cmRjc3FnIn0.ZvgcqlYYmrcZjfinLqY1rA'
