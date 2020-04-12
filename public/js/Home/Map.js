d1676fca3faaaee862cde07b52b65e04de5eacb3
LeeRobb198 <1601432@rgu.ac.uk>
2020-04-12 14:01:51 +0100

    Removal of AR div

--- public/js/Home/Map.js
+++ public/js/Home/Map.js
@@ -48,8 +48,14 @@ function onLocationError(e) {
 map.on('locationfound', onLocationFound);
 map.on('locationerror', onLocationError);
 
-// Add marker for AR element
+// Add marker for AR element North-East Westhill
+// L.marker([57.167843, -2.286872]).addTo(map);
+// Add marker for AR element North Fatima
+// L.marker([57.149092, -2.123048]).addTo(map);
+// Add marker for AR element North-West Mark
 L.marker([57.164314, -2.311392]).addTo(map);
+
+
 
 // Icon ------------------------------------------------------------------------
 
