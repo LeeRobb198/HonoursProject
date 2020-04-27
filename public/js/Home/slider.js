2a8a8f5f42b132dedeceb0715d9a5ba0c5f15048
LeeRobb198 <1601432@rgu.ac.uk>
2020-04-27 19:50:00 +0100

    Slider fixed. Issue was name change to Slider.js rather than slider.js

---
+++ public/js/Home/slider.js
@@ -0,0 +1,23 @@
+/* -------------------------------------------------------------------------- */
+/* Slider JS ---------------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
+
+// Min Slider ------------------------------------------------------------------
+
+var minSlider = document.getElementById("minSlider");
+var minOutput = document.getElementById("minValue");
+minOutput.innerHTML = minSlider.value;
+
+minSlider.oninput = function() {
+  minOutput.innerHTML = this.value;
+}
+
+// Max Slider ------------------------------------------------------------------
+
+var maxSlider = document.getElementById("maxSlider");
+var maxOutput = document.getElementById("maxValue");
+maxOutput.innerHTML = maxSlider.value;
+
+maxSlider.oninput = function() {
+  maxOutput.innerHTML = this.value;
+}
