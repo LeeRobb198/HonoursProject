92cbafb0ac9a83d5f73788ebfad4e64d82fdb571
LeeRobb198 <1601432@rgu.ac.uk>
2020-04-27 19:34:41 +0100

    Change to slider test

--- public/js/Home/slider.js
+++ public/js/Home/slider.js
@@ -4,20 +4,32 @@
 
 // Min Slider ------------------------------------------------------------------
 
-var minSlider = document.getElementById("minSlider");
-var minOutput = document.getElementById("minValue");
-minOutput.innerHTML = minSlider.value;
+// var minSlider = document.getElementById("minSlider");
+// var minOutput = document.getElementById("minValue");
+// minOutput.innerHTML = minSlider.value;
+//
+// minSlider.oninput = function() {
+//   minOutput.innerHTML = this.value;
+// }
 
-minSlider.oninput = function() {
-  minOutput.innerHTML = this.value;
+function updateMinInput(val) {
+
+  var minSlider = document.getElementById("minSlider").value;
+  document.getElementById("minValue").innerHTML = minSlider;
 }
 
 // Max Slider ------------------------------------------------------------------
 
-var maxSlider = document.getElementById("maxSlider");
-var maxOutput = document.getElementById("maxValue");
-maxOutput.innerHTML = maxSlider.value;
+// var maxSlider = document.getElementById("maxSlider");
+// var maxOutput = document.getElementById("maxValue");
+// maxOutput.innerHTML = maxSlider.value;
 
-maxSlider.oninput = function() {
-  maxOutput.innerHTML = this.value;
+// maxSlider.oninput = function() {
+//   maxOutput.innerHTML = this.value;
+// }
+
+function updateMaxInput(val) {
+
+  var maxSlider = document.getElementById("maxSlider").value;
+  document.getElementById("maxValue").innerHTML = maxSlider;
 }
